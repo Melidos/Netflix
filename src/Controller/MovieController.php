69e91ef18454326736b6ea2580b5d7d4ac8a2778
Kevin BESSON <kevin@KBesson.com>
2020-01-17 10:17:37 +0100

    Updated for testing purpose

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -14,6 +14,17 @@ class MovieController extends AbstractController
     {
         return $this->render('movie/index.html.twig', [
             'controller_name' => 'MovieController',
+            'info' => [
+                "background" => "https://image.tmdb.org/t/p/original/zTxHf9iIOCqRbxvl8W5QYKrsMLq.jpg",
+                "name" => "Jumanji: The Next Level",
+                "release_year" => "2019",
+                "synopsis" => "L’équipe est de retour, mais le jeu a changé. Alors qu’ils retournent dans Jumanji pour secourir l’un des leurs, ils découvrent un monde totalement inattendu. Des déserts arides aux montagnes enneigées, les joueurs vont devoir braver des espaces inconnus et inexplorés, afin de sortir du jeu le plus dangereux du monde"
+            ],
+            "similar_movies" => [
+                "1" => "https://image.tmdb.org/t/p/original/zTxHf9iIOCqRbxvl8W5QYKrsMLq.jpg",
+                "Z" => "https://image.tmdb.org/t/p/original/zTxHf9iIOCqRbxvl8W5QYKrsMLq.jpg",
+                "3" => "https://image.tmdb.org/t/p/original/zTxHf9iIOCqRbxvl8W5QYKrsMLq.jpg",
+            ]
         ]);
     }
 }
