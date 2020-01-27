9c929528d3bc96b1836128de95ce49c92c2cec3c
Kevin BESSON <kevin@kbesson.com>
2020-01-27 15:15:31 +0100

    Updated

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -15,43 +15,37 @@ class HomepageController extends AbstractController
     public function getRecommended()
     {
         //TODO: Recuperer automatiquement la liste des ids des films recommandés
-        $idArray = [18, 19, 20, 21, 22, 23, 24, 25];
+        //TODO: Recuperer automatiquement la liste des ids des films populaires pour les utilisateurs non connectés ou ceux n'ayant pas ajouté de films vus
+        $idRec = [18, 19, 20, 21, 22, 23, 24, 25];
 
         //Recuperer les infos des films recommandes par leur id
-        $movieArr = [];
-        foreach ($idArray as $id) {
-            $movieArr[] = $this->getDoctrine()->getRepository(Movie::class)->find($id);
+        $movieRec = [];
+        foreach ($idRec as $id) {
+            $movieRec[] = $this->getDoctrine()->getRepository(Movie::class)->find($id);
         }
+
+        $idPop = [19, 20, 21, 22, 23, 24, 25];
+
+        //Recuperer les infos des films recommandes par leur id
+        $moviePop = [];
+        foreach ($idPop as $id) {
+            $moviePop[] = $this->getDoctrine()->getRepository(Movie::class)->find($id);
+        }
+
+        $idLast = [20, 21, 22, 23, 24, 25];
+
+        //Recuperer les infos des films recommandes par leur id
+        $movieLast = [];
+        foreach ($idLast as $id) {
+            $movieLast[] = $this->getDoctrine()->getRepository(Movie::class)->find($id);
+        }
+
 
         return $this->render('homepage/index.html.twig', [
             'controller_name' => 'HomepageController',
-            "recommended" => $movieArr,
-            "last_watched" => [
-                [
-                    "title" => "Jumanji : next level",
-                    "poster" => "https://image.tmdb.org/t/p/w1280/kc62PCo8u5sZKq0crYvZSidLFPr.jpg",
-                    "release_year" => 2019,
-                    "id" => 1,
-                ],
-                [
-                    "title" => "Star Wars : L'Ascension de Skywalker",
-                    "poster" => "https://image.tmdb.org/t/p/w1280/w1fqnG3W2xqCPTvjdPJTcfPMYH1.jpg",
-                    "release_year" => 2019,
-                    "id" => 1,
-                ],
-                [
-                    "title" => "Joker",
-                    "poster" => "https://image.tmdb.org/t/p/w1280/tWjJ3ILjsbTwKgXxEv48QAbYZ19.jpg",
-                    "release_year" => 2019,
-                    "id" => 1,
-                ],
-                [
-                    "title" => "Once Upon a Time… in Hollywood",
-                    "poster" => "https://image.tmdb.org/t/p/w1280/oaxMobKQgpUbZI0WcNiSq6qRwyA.jpg",
-                    "release_year" => 2019,
-                    "id" => 1,
-                ],
-            ],
+            "popular" => $moviePop,
+            "recommended" => $movieRec,
+            "last_watched" => $movieLast,
         ]);
     }
 }
