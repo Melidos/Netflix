36725c2ef162019f32d78544e743942c6959fbde
Kevin BESSON <kevin@KBesson.com>
2020-01-28 15:10:36 +0100

    Updated

--- src/Command/GetMoviedbDataCommand.php
+++ src/Command/GetMoviedbDataCommand.php
@@ -2,22 +2,29 @@
 
 namespace App\Command;
 
-use App\Entity\Movie;
-
+use DateTime;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Style\SymfonyStyle;
+use App\Entity\Movie;
 
-use Symfony\Component\DependencyInjection\ContainerInterface;
-
-class GetMoviedbDataCommand extends Command
+class GetMovieDbDataCommand extends Command
 {
-    protected static $defaultName = 'GetMoviedbData';
+    protected static $defaultName = 'GetMovieDbData';
 
-    private $container;
+    /**
+     * @var EntityManagerInterface
+     */
+    private $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        parent::__construct();
+        $this->em = $em;
+    }
 
     protected function configure()
     {
@@ -28,60 +35,36 @@ class GetMoviedbDataCommand extends Command
         ;
     }
 
-    public function __construct(ContainerInterface $container)
-    {
-        parent::__construct();
-        $this->container = $container;
-    }
-
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $io = new SymfonyStyle($input, $output);
-        $arg1 = $input->getArgument('arg1');
-
-        /*
-        $entityManager = $this->container->get('doctrine')->getManager();
-
-        $result = $this->container->get('doctrine')->getRepository(Movie::class)->findAll();
-
-        foreach ($result as $key => $value) {
-            echo($value->getMoviedbid());
+        $movies = $this->em->getRepository(Movie::class)->findAll();
+        foreach ($movies as $movie) {
+            $movieIds[] = $movie->getMovieDbId();
         }
-        */
 
+        $lastId = json_decode(file_get_contents("https://api.themoviedb.org/3/movie/latest?api_key=7d43208d19a4ad654a8afdf455744183&language=fr-FR"), true)["id"];
 
-        for ($i=intval($arg1); $i<=659682; $i+=1) {
-            echo("Processing id ".$i." / 659682\n");
-            try {
-                //TODO : arrayOfId = $movie->getMovieDbId()
-                $data = json_decode(@file_get_contents('https://api.themoviedb.org/3/movie/'.$i.'?api_key=7d43208d19a4ad654a8afdf455744183&language=fr-FR'));
+        for ($i = (isset($movieIds) ? max($movieIds) + 1 : 0); $i <= $lastId; $i++) {
+            $movieJson = json_decode(@file_get_contents("https://api.themoviedb.org/3/movie/".$i."?api_key=7d43208d19a4ad654a8afdf455744183&language=fr-FR&append_to_response=fr-FR%2Cen-US%2Cnull"), true);
+            if (isset($movieJson)) {
+                print("id ".$i." traité ".$movieJson["title"].PHP_EOL);
+                $movie = new Movie();
+                $movie->setPoster($movieJson["poster_path"]);
+                $movie->setReleaseDate(new DateTime($movieJson["release_date"]));
+                $movie->setTitle($movieJson["title"]);
+                $movie->setBackground($movieJson["backdrop_path"]);
+                $movie->setMoviedbId($i);
+                $movie->setRuntime($movieJson["runtime"]);
+                $movie->setSynopsis($movieJson["overview"]);
 
-                if (strpos($http_response_header[0], "404") != false || strpos($http_response_header[0], "502") != false) {
-                    continue;
-                }
-                else {
-                    $entityManager = $this->container->get('doctrine')->getManager();
-                    
-                    $movie = new Movie();
-
-                    $name = ($data->title) ? $data->title : $data->original_title ;
-                    
-                    $movie->setName($name);
-                    
-                    $movie->setYear($data->release_date);
-                    $movie->setSynopsis($data->overview);
-                    $movie->setCover('image.tmdb.org/t/p/original'.$data->poster_path);
-                    $movie->setBackground('image.tmdb.org/t/p/original'.$data->backdrop_path);
-                    $movie->setMoviedbid($i);
-
-                    $entityManager->persist($movie);
-
-                    $entityManager->flush();
-                }   
-            } catch (Exception $e) {
-                echo("Error" + $e);
+                $this->em->persist($movie);
+                $this->em->flush();
+            }
+            else {
+                print("id ".$i." non présent".PHP_EOL);
             }
         }
+
         return 0;
     }
 }
